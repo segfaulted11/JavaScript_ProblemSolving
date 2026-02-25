070605842a8e7a419db39472f792c31c4a184aea
Seg Fault <segfaulted11@gmail.com>
2026-02-25 16:24:56 +0600

    remove duplicate from the array

---
+++ js.js
@@ -0,0 +1,16 @@
+let array = [11,12,13,14,13,13,15,15];
+
+function noDuplicate(parameterArray){
+
+    let newArray = [];
+
+    for(let eachNumber of array){
+        if (newArray.includes(eachNumber)===false){
+            newArray.push(eachNumber)
+        }
+    }
+return newArray;//[ 11, 12, 13, 14, 15 ]
+}
+
+let xyz = noDuplicate(array)
+console.log(xyz)
